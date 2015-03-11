7f0b1b7df7383340f6fc77a7052f4e3e0a47aa7c
AlecLangford <git@alec.id.au>
2015-03-11 18:04:53 +1100

    Init
    The ReWrite

---
+++ sitrep.mako
@@ -0,0 +1,42 @@
+
+<html>
+<head>
+
+<style>
+    @page {
+        size: A4 portrait;
+        @frame content_frame {
+            left: 50pt;
+            width: 512pt;
+            top: 50pt;
+            height: 692pt;
+            -pdf-frame-border: 1;    /* for debugging the layout */
+        }
+        margin: 2cm;
+    }
+</style>
+
+    </head>
+
+<body>
+MARCH PA03-ROMEO-11 SITREP
+Our Canberra and surrounds (Cell PA03-ROMEO-11) March 2015 First Saturday #IngressFS started at the Rivett Community Park and end at the National Arboretum, ACT, Australia. We had 31 agents register.
+It was a glorious day for ingress in Australia’s capital. We kicked stuff off in style donning ingress tattoos, starting our FS mission and saying g’day to each other.
+Agents were invited this month to donate old computer games to support Lifeline. A huge thank you to the agents who donated. Some real classics are going to a great new home. #Missionsforgood
+We wrapped up our day up enjoying the Canberra skyline views at the National Arboretum. To mix things up agents discovered that the glyph on their name tag had a matching one on the opposing faction tags. The challenge was set to find your counterpart and take a FS selfie which would earn you some swag. #Ingressmiles
+Congratulations to our fs trophy holder for this month: Enlightened:@ojutai gaining 3 levels. Our overall winning faction was: Enlightened
+With a total of 1,222,761 AP
+An average AP: 87,340
+And a total 8 levels gained
+The Resistance scored a total 1,376,118 AP with an average 80,961 and gained a total of 2 levels.
+Congratulations to our swag winners for this month Enlightened: @yoko09 for most Enlightened hacks with 111 Resistance: @Cerri for most Resistance hacks with 222 Resistance: @Cerri & @Poly with a total of 6 Kms. AP decided win to @Cerri Enlightened: @RoboticUnicorn & @Punktuality with a total of 4 Kms. AP decided win to @RoboticUnicorn Total AP captured: 2,437,190
+Total Levels gained for the day: 10
+Total hacks for the day: 1905
+Total kms covered by agents: 71km
+Well done to all the agents involved hope to see you again next month
+https://plus.google.com/events/ctsskqo7a76fj0t2p1scrahd2sc
+
+TRANSMISSION TERMINATED.
+
+</body>
+</html>
---
+++ websvr.py
@@ -0,0 +1,246 @@
+import spelling
+from peewee import *
+import requests
+from cherrypy.lib.static import serve_file
+from mako.template import Template
+from mako.lookup import TemplateLookup
+import cherrypy.process.plugins
+import cherrypy
+
+class DBM():
+    def __init__(self):
+        self.db = SqliteDatabase(':memory:', threadlocals=True)
+
+dbm = DBM()
+
+
+class player_begin(Model):
+
+    PlayerName = CharField(unique=True)
+    Faction = CharField(unique=False)
+    StartLevel = IntegerField()
+    StartAP = IntegerField()
+    StartStat1 = IntegerField()
+    StartStat2 = IntegerField()
+
+    class Meta:
+        database = dbm.db
+
+class player_end(Model):
+
+    PlayerName = CharField(unique=True)
+    EndLevel = IntegerField()
+    EndAP = IntegerField()
+    EndStat1 = IntegerField()
+    EndStat2 = IntegerField()
+
+    class Meta:
+        database = dbm.db
+
+class player(Model):
+    PlayerName = CharField(unique=True)
+    Faction = CharField(unique=False)
+    DiffLevel = IntegerField()
+    DiffAP = IntegerField()
+    DiffStat1 = IntegerField()
+    DiffStat2 = IntegerField()
+
+    class Meta:
+        database = dbm.db
+
+
+dbm.db.create_tables([player_begin, player_end, player])
+
+class DataMGR():
+    def __init__(self,stat1="Kms",stat2="Hacks"):
+        self.template_url = "https://docs.google.com/spreadsheets/d/%s/export?format=csv"
+        signinurl = "https://docs.google.com/spreadsheets/d/1b6hcUfZv4JO1qNxPMsWm0eiVuuaF0JIndlijvzCYDhM/pubhtml"
+        signouturl = "https://docs.google.com/spreadsheets/d/1n6m-QXZeZ15JczhNsc5c8hO-44DOOgB1cEU96BlvgMY/pubhtml"
+        self.signin = ''
+        self.signout = ''
+        self.urls = [signinurl, signouturl]
+        self.correct_users = ''
+        self.begin = ['PlayerName','Faction','StartLevel','StartAP','StartStat1','StartStat2']
+        self.end = ["PlayerName","EndLevel","EndAP","EndStat1","EndStat2"]
+        self.joined = ["PlayerName","Faction", "DiffLevel" ,"DiffAP" ,"DiffStat1","DiffStat2"]
+        self.stat1 = stat1
+        self.stat2 = stat2
+        self.scoreboard = []
+
+    def get_key(self,x):
+        x = x[39:].split("/")
+        return x[0]
+
+    def get_data(self,url):
+        response = requests.get(url)
+        return response.text.split("\n")[1:]
+
+    def collect(self):
+
+        t=[]
+        for item in self.urls:
+            i = self.template_url%self.get_key(item)
+            t.append(self.get_data(i))
+        self.signin, self.signout = t
+
+
+        for item in self.signin:
+            item = item.split(",")
+            self.correct_users+=(item[1]+"\n")
+            i=[item[1].lower(),item[2]]+map(int,item[3:])
+            try:
+                c = player_begin.create(**dict(zip(self.begin, i)))
+            except IntegrityError:
+                pass
+        correct = spelling.main(self.correct_users)
+
+        for item in self.signout:
+            item = item.split(",")
+            i = [correct(item[1]), ]+ map(int,item[2:])
+            try:
+                c = player_end.create(**dict(zip(self.end, i)))
+            except IntegrityError:
+                pass
+
+    def join_tables(self):
+        f=player._meta.get_field_names()[1:]
+        for item in player_begin.raw("select * from player_begin LEFT JOIN player_end ON player_begin.PlayerName=player_end.PlayerName"):
+            try:
+                r=[item.PlayerName, item.Faction, item.EndLevel-item.StartLevel, item.EndAP-item.StartAP, item.EndStat1-item.StartStat1, item.EndStat2-item.StartStat2]
+                c = player.create(**dict(zip(self.joined, r)))
+            except TypeError:
+                pass
+
+        qry = player.select().order_by(player.DiffLevel.desc(), player.DiffAP.desc())
+        results=open("scoreboard.csv","w")
+        self.scoreboard=[]
+        for item in qry:
+            x=[str(getattr(item,i)) for i in f]
+            results.write(",".join(x)+"\n")
+            self.scoreboard.append(x)
+        results.close()
+        #print self.scoreboard
+
+        qry1 = player.select().where(player.Faction=="Enlightened")
+        enlightened_counted = qry1.count()
+        if enlightened_counted ==0:
+            enlightened_counted+=1
+        enlightened_average = sum([item.DiffAP for item in qry1])/enlightened_counted
+        enlightened_total = sum([item.DiffAP for item in qry1] )
+        enlightened_levels = sum([item.DiffLevel for item in qry1] )
+        enlightened_stat1_total = sum([item.DiffStat1 for item in qry1] )
+        enlightened_stat1_avg = sum([item.DiffStat1 for item in qry1] )/enlightened_counted
+        enlightened_stat2_total = sum([item.DiffStat1 for item in qry1] )
+        enlightened_stat2_avg = sum([item.DiffStat1 for item in qry1] )/enlightened_counted
+
+        qry1 = player.select().where(player.Faction=="Resistance")
+        resistance_counted = qry1.count()
+        if resistance_counted == 0:
+            resistance_counted +=1
+        resistance_average = sum([item.DiffAP for item in qry1])/resistance_counted
+        resistance_total = sum([item.DiffAP for item in qry1] )
+        resistance_levels = sum([item.DiffLevel for item in qry1] )
+        resistance_stat1_total = sum([item.DiffStat1 for item in qry1] )
+        resistance_stat1_avg = sum([item.DiffStat1 for item in qry1] )/enlightened_counted
+        resistance_stat2_total = sum([item.DiffStat2 for item in qry1] )
+        resistance_stat2_avg = sum([item.DiffStat2 for item in qry1] )/enlightened_counted
+
+        d={
+            "e_players": enlightened_counted,
+            "e_average": enlightened_average,
+            "e_total": enlightened_total,
+            "e_levels": enlightened_levels,
+            "e_stat1_total": enlightened_stat1_total,
+            "e_stat1_avg": enlightened_stat1_avg,
+            "e_stat2_total": enlightened_stat2_total,
+            "e_stat2_avg": enlightened_stat2_avg,
+
+
+            "r_players": resistance_counted,
+            "r_average": resistance_average,
+            "r_total": resistance_total,
+            "r_levels": resistance_levels,
+            "r_stat1_total": resistance_stat1_total,
+            "r_stat1_avg": resistance_stat1_avg,
+            "r_stat2_total": resistance_stat2_total,
+            "r_stat2_avg": resistance_stat2_avg,
+
+
+            "g_scoreboard": self.scoreboard,
+            "g_players": sum([enlightened_counted,resistance_counted]),
+            "g_stat1Name":self.stat1,
+            "g_stat2Name":self.stat2,
+            "g_stat1_total":sum([resistance_stat1_total,enlightened_stat1_total]),
+            "g_stat2_total":sum([resistance_stat2_total,enlightened_stat2_total]),
+
+        }
+        return d
+
+    def update_scoreboard(self):
+        self.collect()
+        d=self.join_tables()
+        return d
+
+
+d=DataMGR()
+
+class WebApp(object):
+    def __init__(self):
+        d.collect()
+        self.lookup = TemplateLookup(directories=["/"])
+
+    @cherrypy.expose
+    def index(self):
+
+        t = Template("index.mako",lookup=self.lookup)
+        return t.render()
+
+    @cherrypy.expose
+    def sitrep(self):
+
+        t = Template("sitrep.mako")
+        r=d.join_tables()
+        return t.render(**r)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+d.collect()
+x=d.join_tables()
+print x
+for i, item in enumerate(x["g_scoreboard"]):
+    print i+1,item"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
